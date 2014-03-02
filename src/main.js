2512b4a7a0b65c55cd59edfadf964d88dcf5c5c3
Michael Kosler <marekkpie@gmail.com>
2014-03-01 19:08:25 -0600

    Removed comments and removed test file

--- src/main.js
+++ src/main.js
@@ -1,52 +1,7 @@
 $(document).ready(function () {
-    /*
-    $.ajax({
-        url: "test_property.json",
-
-        type: "GET",
-
-        dataType: "json",
-
-        success: function (result) {
-            console.log("Success!!");
-            console.log(result);
-        },
-
-        error: function (request, textStatus, errorThrown) {
-            console.log("Error!!");
-            console.log(textStatus);
-        },
-
-        complete: function (request, textStatus) {
-            console.log("Complete!!");
-            console.log(request.responseText);
-            console.log(textStatus);
-        },
-    });
-    */
-
-    /*
-    $.getJSON("test_property.json", function (data) {
-        console.log("Kappa");
-    });
-    */
-
     var property = Property.fromJSON("test_property.json");
 
     $("body").append(property.renderer.domElement);
-
-    /*
-    $(".texture").each(function () {
-        property.addRoom(new Room(THREE.ImageUtils.loadTexture(this.src)));
-    });
-
-    property.setCurrentRoom(0);
-
-    var DOOR_ROUNDED_TEXTURE = THREE.ImageUtils.loadTexture($("#door")[0].src),
-        connection = new Connection(new THREE.Vector3(0, 0, -10), 0, DOOR_ROUNDED_TEXTURE);
-
-    sphere.add(connection);
-    */
 
     property.bind();
 
